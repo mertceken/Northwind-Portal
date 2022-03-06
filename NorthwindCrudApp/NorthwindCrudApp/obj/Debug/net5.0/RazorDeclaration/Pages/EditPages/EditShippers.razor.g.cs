7def5c8eba56ff34ecfab20aa1b80aa5d92970ba
mert çeken <mert.ceken12@gmail.com>
2022-03-06 23:56:26 +0300

    İlk Git Bash testi.

--- NorthwindCrudApp/NorthwindCrudApp/obj/Debug/net5.0/RazorDeclaration/Pages/EditPages/EditShippers.razor.g.cs
+++ NorthwindCrudApp/NorthwindCrudApp/obj/Debug/net5.0/RazorDeclaration/Pages/EditPages/EditShippers.razor.g.cs
@@ -13,77 +13,77 @@ namespace NorthwindCrudApp.Pages.EditPages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 1 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 2 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 3 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 4 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 5 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 6 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 7 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 8 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using NorthwindCrudApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 9 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using NorthwindCrudApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
+#line 10 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\_Imports.razor"
 using MatBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\Pages\EditPages\EditShippers.razor"
+#line 3 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\Pages\EditPages\EditShippers.razor"
 using NorthwindCrudApp.Data;
 
 #line default
@@ -98,7 +98,7 @@ using NorthwindCrudApp.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 18 "C:\Users\User\source\repos\NorthwindCrudApp\NorthwindCrudApp\Pages\EditPages\EditShippers.razor"
+#line 18 "C:\Users\User\Documents\GitHub\Northwind-Portal\NorthwindCrudApp\NorthwindCrudApp\Pages\EditPages\EditShippers.razor"
        
     Shippers shippers = new Shippers();
 
